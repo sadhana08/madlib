b406c6d991d783fd32f8b79746432085739c99d6
sadhana <sadhanasuresh2611@gmail.com>
2021-03-10 22:17:19 +0530

    first commit

---
+++ python projects youtube/madlib.py
@@ -0,0 +1,11 @@
+# Madlib game
+place = input("Enter your favourite place:")
+
+country = input("Enter a country's name:")
+
+noun = input("Enter a noun:")
+
+madlib = print(f"What a beautiful {place}. You are going to live in {country}.\
+My favourite character is {noun} ")
+
+print (madlib)
