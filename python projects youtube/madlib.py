94d989c07e303a374c97213b4621801489324939
sadhana08 <79145459+sadhana08@users.noreply.github.com>
2021-03-10 22:37:43 +0530

    Add files via upload
    Two print statements were given for a same operation . Thus removed one.

--- python projects youtube/madlib.py
+++ python projects youtube/madlib.py
@@ -1,11 +1,7 @@
 # Madlib game
 place = input("Enter your favourite place:")
-
 country = input("Enter a country's name:")
-
 noun = input("Enter a noun:")
-
-madlib = print(f"What a beautiful {place}. You are going to live in {country}.\
-My favourite character is {noun} ")
-
-print (madlib)
+madlib = f"What a beautiful {place}. You are going to live in {country}.\
+My favourite character is {noun} "
+print(madlib)
